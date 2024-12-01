a0efe547b77aced8f4d4c9e3c45d43199e1203d5
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-30 21:17:47 -0500

    Test: Created a function to send an HTTP GET to request to a specific URL.

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
 import requests
 import pytest
+
+def test_http_get():
+    url = "https://httpbin.org/get"
+    response = send_http_get_request(url)
+    assert isinstance(response, tuple)
+    assert response[0] == 200 # 200 :Request from a client to a server was successful
