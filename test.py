865048d8f8728ef1550d7502702b1a5f87332aa2
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-30 22:06:32 -0500

    Imported: Import the send_post_to_beeceptor() function in the test function

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 import pytest
-from main import send_http_get_request, get_beeceptor_data
+from main import send_http_get_request, get_beeceptor_data, send_post_to_beeceptor
 
 def test_http_get():
     url = "https://httpbin.org/get"
