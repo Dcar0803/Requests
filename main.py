f145eaa7fd404e2e9da07d81f1c6d3bbb425fc60
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-30 21:33:08 -0500

    Function: Created a function to send an HTTP GET to request to a specific URL and returns the status code and the response text

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import requests
+
+def send_http_get_request(url):
+    response = requests.get(url)
+    if 400 <= response.status_code <= 499:
+        raise Exception(f"Client error {response.status_code}: {response.reason}")
+    if "application/json" in response.headers.get("Content-Type", ""):
+        return response.status_code, response.json()
+    return response.status_code, response.text
