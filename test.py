6e8277c2076d00ffbda7530eecdf82428ca062f8
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-30 21:44:19 -0500

    Test: Created the test function to send a get request and print return a tuple of the Postman-Token and IP address.

--- test.py
+++ test.py
@@ -7,3 +7,9 @@ def test_http_get():
     response = send_http_get_request(url)
     assert isinstance(response, tuple)
     assert response[0] == 200 # 200 :Request from a client to a server was successful
+
+
+def test_get_beeceptor():
+    response = get_beeceptor_data()
+    assert isinstance(response, tuple)  # Need to return a tuple.
+    assert len(response) == 2  # the tuple contains two elements.
